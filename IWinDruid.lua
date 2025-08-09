00be50544a13a3d573d801c85e9fa7eaee046eb7
Agamemnoth <Profiler781@users.noreply.github.com>
2025-08-09 12:08:57 +0200

    v1.1

---
+++ IWinDruid.lua
@@ -0,0 +1,425 @@
+--[[
+#########################################
+# IWinDruid Discord Agamemnoth#5566  #
+#########################################
+]]--
+
+---- For Druids ----
+if UnitClass("player") ~= "Druid" then return end
+
+---- Loading ----
+IWin = CreateFrame("frame",nil,UIParent)
+IWin.t = CreateFrame("GameTooltip", "IWin_T", UIParent, "GameTooltipTemplate")
+local IWin_Settings = {
+	["rageTimeToReserveBuffer"] = 1.5,
+	["ragePerSecondPrediction"] = 10, -- change it to match your gear and buffs
+}
+local IWin_CombatVar = {
+	["dodge"] = 0,
+	["reservedRage"] = 0,
+	["reservedRageStance"] = nil,
+	["charge"] = 0,
+}
+local Cast = CastSpellByName
+
+---- Event Register ----
+IWin:RegisterEvent("CHAT_MSG_COMBAT_SELF_MISSES")
+IWin:RegisterEvent("CHAT_MSG_SPELL_DAMAGESHIELDS_ON_SELF")
+IWin:RegisterEvent("ADDON_LOADED")
+IWin:SetScript("OnEvent", function()
+	if event == "ADDON_LOADED" and arg1 == "IWinDruid" then
+		DEFAULT_CHAT_FRAME:AddMessage("|cff0066ff IWinDruid system loaded.|r")
+		IWin:UnregisterEvent("ADDON_LOADED")
+	elseif event == "CHAT_MSG_COMBAT_SELF_MISSES" or event == "CHAT_MSG_SPELL_DAMAGESHIELDS_ON_SELF" then
+		if string.find(arg1,"dodge") then
+			IWin_CombatVar["dodge"] = GetTime()
+		end
+	end
+end)
+
+---- Spell data ----
+function IWin:GetTalentRank(tabIndex, talentIndex)
+	local _, _, _, _, currentRank = GetTalentInfo(tabIndex, talentIndex)
+	return currentRank
+end
+
+IWin_RageCost = {
+	
+}
+
+IWin_Taunt = {
+	"Taunt",
+	"Mocking Blow",
+	"Challenging Shout",
+	"Growl",
+	"Challenging Roar",
+	"Hand of Reckoning",
+}
+
+---- Functions ----
+function IWin:GetBuffIndex(unit, spell)
+	local index = 1
+	while UnitBuff(unit, index) do
+		IWin_T:SetOwner(WorldFrame, "ANCHOR_NONE")
+		IWin_T:ClearLines()
+		IWin_T:SetUnitBuff(unit, index)
+		local buffName = IWin_TTextLeft1:GetText()
+		if buffName == spell then
+			return index
+		end
+		index = index + 1
+	end
+	return nil
+end
+
+function IWin:GetDebuffIndex(unit, spell)
+	index = 1
+	while UnitDebuff(unit, index) do
+		IWin_T:SetOwner(WorldFrame, "ANCHOR_NONE")
+		IWin_T:ClearLines()
+		IWin_T:SetUnitDebuff(unit, index)
+		local buffName = IWin_TTextLeft1:GetText()
+		if buffName == spell then 
+			return index
+		end
+		index = index + 1
+	end	
+	return nil
+end
+
+function IWin:GetBuffStack(unit, spell)
+	local index = IWin:GetBuffIndex(unit, spell)
+	if index then
+		local _, stack = UnitBuff(unit, index)
+		return stack
+	end
+	local index = IWin:GetDebuffIndex(unit, spell)
+	if index then
+		local _, stack = UnitDebuff(unit, index)
+		return stack
+	end
+	return 0
+end
+
+function IWin:IsBuffStack(unit, spell, stack)
+	return IWin:GetBuffStack(unit, spell) == stack
+end
+
+function IWin:IsBuffActive(unit, spell)
+	return IWin:GetBuffStack(unit, spell) ~= 0
+end
+
+function IWin:GetBuffRemaining(unit, spell)
+	if unit == "player" then
+		local index = IWin:GetBuffIndex(unit, spell)
+		if index then
+			return GetPlayerBuffTimeLeft(index - 1)
+		end
+		local index = IWin:GetDebuffIndex(unit, spell)
+		if index then
+			return GetPlayerBuffTimeLeft(index - 1)
+		end
+	elseif unit == "target" then
+		local libdebuff = pfUI and pfUI.api and pfUI.api.libdebuff or ShaguTweaks and ShaguTweaks.libdebuff
+		if not libdebuff then
+	    	DEFAULT_CHAT_FRAME:AddMessage("Either pfUI or ShaguTweaks required")
+	    	return 0
+		end
+		local index = IWin:GetDebuffIndex(unit, spell)
+		if index then
+			local _, _, _, _, _, _, timeleft = libdebuff:UnitDebuff("target", index)
+			return timeleft
+		end
+	end
+	return 0
+end
+
+function IWin:GetCooldownRemaining(spell)
+	local spellID = 1
+	local bookspell = GetSpellName(spellID, "BOOKTYPE_SPELL")
+	while bookspell do	
+		if spell == bookspell then
+			local start, duration = GetSpellCooldown(spellID, "BOOKTYPE_SPELL")
+			if start ~= 0 and duration ~= 1.5 then
+				return duration - (GetTime() - start)
+			else
+				return 0
+			end
+		end
+		spellID = spellID + 1
+		bookspell = GetSpellName(spellID, "BOOKTYPE_SPELL")
+	end
+	return false
+end
+
+function IWin:IsOnCooldown(spell)
+	return IWin:GetCooldownRemaining(spell) ~= 0
+end
+
+function IWin:IsSpellLearnt(spell)
+	local spellID = 1
+	local bookspell = GetSpellName(spellID, "BOOKTYPE_SPELL")
+	while bookspell do
+		if bookspell == spell then
+			return true
+		end
+		spellID = spellID + 1
+		bookspell = GetSpellName(spellID, "BOOKTYPE_SPELL")
+	end
+	return false
+end
+
+function IWin:IsCharging()
+	local chargeTimeActive = GetTime() - IWin_CombatVar["charge"]
+	return chargeTimeActive < 1
+end
+
+function IWin:IsStanceActive(stance)
+	for index = 1, 3 do
+		local _, name, active = GetShapeshiftFormInfo(index)
+		if name == stance then
+			return active == 1
+		end
+	end
+	return false
+end
+
+function IWin:GetHealthPercent(unit)
+	return UnitHealth(unit) / UnitHealthMax(unit)
+end
+
+function IWin:IsExecutePhase()
+	return IWin:GetHealthPercent("target") <= 0.2
+end
+
+function IWin:IsRageAvailable(spell)
+	local rageRequired = IWin_RageCost[spell] + IWin_CombatVar["reservedRage"]
+	return UnitMana("player") >= rageRequired
+end
+
+function IWin:IsRageCostAvailable(spell)
+	return UnitMana("player") >= IWin_RageCost[spell]
+end
+
+function IWin:IsInMeleeRange()
+	return CheckInteractDistance("target", 3) ~= nil
+end
+
+function IWin:GetRageToReserve(spell, trigger, unit)
+	local spellTriggerTime = 0
+	if trigger == "nocooldown" then
+		return IWin_RageCost[spell]
+	elseif trigger == "cooldown" then
+		spellTriggerTime = IWin:GetCooldownRemaining(spell) or 0
+	elseif trigger == "buff" or trigger == "partybuff" then
+		spellTriggerTime = IWin:GetBuffRemaining(unit, spell) or 0
+	end
+	local reservedRageTime = 0
+	if IWin_Settings["ragePerSecondPrediction"] > 0 then
+		reservedRageTime = IWin_CombatVar["reservedRage"] / IWin_Settings["ragePerSecondPrediction"]
+	end
+	local timeToReserveRage = math.max(0, spellTriggerTime - IWin_Settings["rageTimeToReserveBuffer"] - reservedRageTime)
+	if trigger == "partybuff" or IWin:IsSpellLearnt(spell) then
+		return math.max(0, IWin_RageCost[spell] - IWin_Settings["ragePerSecondPrediction"] * timeToReserveRage)
+	end
+	return 0
+end
+
+function IWin:IsTimeToReserveRage(spell, trigger, unit)
+	return IWin:GetRageToReserve(spell, trigger, unit) ~= 0
+end
+
+function IWin:SetReservedRage(spell, trigger, unit)
+	IWin_CombatVar["reservedRage"] = IWin_CombatVar["reservedRage"] + IWin:GetRageToReserve(spell, trigger, unit)
+end
+
+function IWin:IsTanking()
+	return UnitIsUnit("targettarget", "player")
+end
+
+function IWin:GetItemID(itemLink)
+	for itemID in string.gfind(itemLink, "|c%x+|Hitem:(%d+):%d+:%d+:%d+|h%[(.-)%]|h|r$") do
+		return itemID
+	end
+end
+
+IWin_UnitClassification = {
+	["worldboss"] = true,
+	["rareelite"] = true,
+	["elite"] = true,
+	["rare"] = false,
+	["normal"] = false,
+	["trivial"] = false,
+}
+
+function IWin:IsElite()
+	local classification = UnitClassification("target")
+	return IWin_UnitClassification[classification]
+end
+
+function IWin:IsRageReservedStance(stance)
+	if IWin_CombatVar["reservedRageStance"] then
+		return IWin_CombatVar["reservedRageStance"] == stance
+	end
+	return true
+end
+
+function IWin:IsTaunted()
+	local index = 1
+	while IWin_Taunt[index] do
+		local taunt = IWin:IsBuffActive("target", IWin_Taunt[index])
+		if taunt then
+			return true
+		end
+		index = index + 1
+	end
+	return false
+end
+
+---- Actions ----
+function IWin:TargetEnemy()
+	if not UnitExists("target") or UnitIsDead("target") or UnitIsFriend("target", "player") then
+		TargetNearestEnemy()
+	end
+end
+
+function IWin:StartAttack()
+	local attackActionFound = false
+	for action = 1, 172 do
+		if IsAttackAction(action) then
+			attackActionFound = true
+			if not IsCurrentAction(action) then
+				UseAction(action)
+			end
+		end
+	end
+	if not attackActionFound and not PlayerFrame.inCombat then
+		AttackTarget()
+	end
+end
+
+function IWin:MarkOfTheWild()
+	if IWin:IsSpellLearnt("Mark of the Wild") and IWin:GetBuffRemaining("player","Mark of the Wild") < 60 and not UnitAffectingCombat("player") then
+		Cast("Mark of the Wild")
+	end
+end
+
+function IWin:Moonfire()
+	if IWin:IsSpellLearnt("Moonfire") and not IWin:IsBuffActive("target", "Moonfire") then
+		Cast("Moonfire")
+	end
+end
+
+function IWin:Thorns()
+	if IWin:IsSpellLearnt("Thorns") and IWin:GetBuffRemaining("player","Thorns") < 60 and not UnitAffectingCombat("player") then
+		Cast("Thorns")
+	end
+end
+
+function IWin:Wrath()
+	if IWin:IsSpellLearnt("Wrath") then
+		Cast("Wrath")
+	end
+end
+
+function IWin:WrathOOC()
+	if IWin:IsSpellLearnt("Wrath") and not UnitAffectingCombat("player") then
+		Cast("Wrath")
+	end
+end
+
+---- idebug button ----
+SLASH_IDEBUG1 = '/idebug'
+function SlashCmdList.IDEBUG()
+	--DEFAULT_CHAT_FRAME:AddMessage()
+	IWin:IsTaunted()
+end
+
+---- iblast button ----
+SLASH_IBLAST1 = '/iblast'
+function SlashCmdList.IBLAST()
+	IWin_CombatVar["reservedRage"] = 0
+	IWin_CombatVar["reservedRageStance"] = nil
+	IWin:TargetEnemy()
+	IWin:MarkOfTheWild()
+	IWin:Thorns()
+	IWin:WrathOOC()
+	IWin:Moonfire()
+	IWin:Wrath()
+end
+
+---- istorm button ----
+SLASH_ISTORM1 = '/istorm'
+function SlashCmdList.ISTORM()
+	IWin_CombatVar["reservedRage"] = 0
+	IWin_CombatVar["reservedRageStance"] = nil
+	IWin:TargetEnemy()
+
+end
+
+---- iruetoo button ----
+SLASH_IRUETOO1 = '/iruetoo'
+function SlashCmdList.IRUETOO()
+	IWin_CombatVar["reservedRage"] = 0
+	IWin_CombatVar["reservedRageStance"] = nil
+	IWin:TargetEnemy()
+
+	IWin:StartAttack()
+end
+
+---- isacat button ----
+SLASH_ISACAT1 = '/isacat'
+function SlashCmdList.ISACAT()
+	IWin_CombatVar["reservedRage"] = 0
+	IWin_CombatVar["reservedRageStance"] = nil
+	IWin:TargetEnemy()
+
+	IWin:StartAttack()
+end
+
+---- itank button ----
+SLASH_ITANK1 = '/itank'
+function SlashCmdList.ITANK()
+	IWin_CombatVar["reservedRage"] = 0
+	IWin_CombatVar["reservedRageStance"] = nil
+	IWin:TargetEnemy()
+
+	IWin:StartAttack()
+end
+
+---- ihodor button ----
+SLASH_IHODOR1 = '/ihodor'
+function SlashCmdList.IHODOR()
+	IWin_CombatVar["reservedRage"] = 0
+	IWin_CombatVar["reservedRageStance"] = nil
+	IWin:TargetEnemy()
+
+	IWin:StartAttack()
+end
+
+---- ichase button ----
+SLASH_ICHASE1 = '/ichase'
+function SlashCmdList.ICHASE()
+	IWin:TargetEnemy()
+	IWin:Charge()
+	IWin:Intercept()
+	IWin:Hamstring()
+	IWin:StartAttack()
+end
+
+---- ikick button ----
+SLASH_IKICK1 = '/ikick'
+function SlashCmdList.IKICK()
+	IWin:TargetEnemy()
+	IWin:ShieldBash()
+	IWin:Pummel()
+	IWin:StartAttack()
+end
+
+---- itaunt button ----
+SLASH_ITAUNT1 = '/itaunt'
+function SlashCmdList.ITAUNT()
+	IWin:TargetEnemy()
+	
+	IWin:StartAttack()
+end
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # IWinDruid
+
 Smart macros for Turtle Druids. Make macros with commands. Put them on your action bars. Enjoy!
+
+
+## Commands
+    /idps        Single target DPS rotation
+    /icleave     Multi target DPS rotation
+    /itank       Single target threat rotation
+    /ihodor      Multi target threat rotation
+    /ichase      Stick to your target with Charge, Intercept, Hamstring
+    /ikick       Kick with Pummel or Shield Bash
+    /ifeardance  Use Berserker Rage if available
+    /itaunt      Taunt or Mocking Blow if the target is not under another taunt effect
+
+## Required Addons
+You need one of the following addons:
+* [pfUI](https://shagu.org/pfUI/), A full UI replacement.
+* [ShaguTweaks](https://shagu.org/ShaguTweaks/), A non-intrusive quality of life addon.
